e544222c63a27c07b7b8ef0e8f7c91c0903bb6fe
nyount14 <90732548+nyount14@users.noreply.github.com>
2021-10-15 20:28:11 -0500

    practice exercise #3

---
+++ src/app/paragraph-list/paragraph-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paragraph-list',
+  templateUrl: './paragraph-list.component.html',
+  styleUrls: ['./paragraph-list.component.css']
+})
+
+export class ParagraphComponent {
+
+}
